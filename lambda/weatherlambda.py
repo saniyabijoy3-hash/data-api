0bfa4b07704853d484846f093f2e30c31a193566
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-05-14 16:02:11 +0530

    Fetch weather data from OpenWeather API

--- lambda/weatherlambda.py
+++ lambda/weatherlambda.py
@@ -7,5 +7,14 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Weathertable')
 
 def lambda_handler(event, context):
-
     
+     api_key = "2896b53ee1b1dc1840a13a676b798698"
+
+city = "Kochi"
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = urllib.request.urlopen(url)
+
+data = json.loads(response.read())
+ 
