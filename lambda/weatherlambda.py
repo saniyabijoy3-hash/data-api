6753ff605d74e2dc0d67a30e4357c5b5a4c115fb
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-05-14 16:05:52 +0530

    Store weather data in DynamoDB and return response

--- lambda/weatherlambda.py
+++ lambda/weatherlambda.py
@@ -17,10 +17,14 @@ url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}
 response = urllib.request.urlopen(url)
 
 data = json.loads(response.read())
+
 weather_data = {
         "city": data["name"],
         "temperature": str(data["main"]["temp"]),
         "humidity": str(data["main"]["humidity"]),
         "weather_condition": data["weather"][0]["main"]
-    }
+}
 
+table.put_item(Item=weather_data)
+
+return weather_data
