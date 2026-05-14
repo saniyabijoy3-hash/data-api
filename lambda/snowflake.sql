4cebde213fa26f2734ee808ad6ebb9a62a9b432e
saniyabijoy3-hash <saniyabijoy3@gmail.com>
2026-05-14 16:25:46 +0530

    Create Snowflake stage for S3 weather data integration

--- lambda/snowflake.sql
+++ lambda/snowflake.sql
@@ -6,3 +6,11 @@ USE SCHEMA weather1_schema;
 
 CREATE OR REPLACE FILE FORMAT weather_json
 TYPE = JSON;
+
+CREATE OR REPLACE STAGE my_stage
+URL = 's3://weatherdata9/'
+CREDENTIALS = (
+    AWS_KEY_ID = os.getenv('AWS_KEY_ID'),
+    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+)
+FILE_FORMAT = weather_json;
